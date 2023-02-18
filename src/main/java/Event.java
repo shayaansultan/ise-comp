715401b00fdde2d8df9b638d835bda5ae963328d
Tahsin <tahsinhasem@gmail.com>
2023-02-19 02:30:18 +0800

    Added Event.java abstract class

---
+++ src/main/java/Event.java
@@ -0,0 +1,30 @@
+abstract class Event implements Comparable<Event>{
+
+  private final int time;
+
+  public Event(int time) {
+    this.time = time;
+  }
+
+  @Override
+  public int compareTo(Event e) {
+    if (this.time > e.time) {
+      return 1;
+    } else if (this.time == e.time) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+
+  public int getTime() {
+    return time;
+  }
+
+  @Override
+  public String toString(){
+    return String.format("%.3f",this.time);
+  }
+
+  public abstract Event[] simulate();
+}
--- src/main/java/Micron.java
+++ src/main/java/Micron.java
@@ -1,3 +1,13 @@
 public class Micron {
 
+  public static int lots_produced;
+
+
+
+  public Micron(){
+    lots_produced = 0;
+
+  }
+
+
 }
