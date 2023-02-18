528739d23a45bb42f7ca0a241da50f62e9756946
Shayaan Sultan <sultan.shayaan@gmail.com>
2023-02-19 03:20:19 +0800

    Workstation Updated

--- src/main/java/Workstation.java
+++ src/main/java/Workstation.java
@@ -1,3 +1,25 @@
-public class Workstation {
+import java.util.ArrayList;
+import java.util.Queue;
 
+public abstract class Workstation {
+    private String workstationId ;
+    private boolean status = true ;
+    
+
+    public Workstation(String id) {
+        this.workstationId = id ;
+    }
+    
+    public void setAvail(boolean c) {
+        this.status = c;
+    }
+
+    public boolean getAvail() {
+        return this.status;
+    }
+
+    @Override
+    public String toString() {
+        return this.workstationId ;
+    }
 }
